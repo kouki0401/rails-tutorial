7b165e09aecc2055841e83175f78b527ba016096
kohki <kohki4115@gmail.com>
2020-05-17 09:41:43 +0000

    Finish basic login

---
+++ app/helpers/sessions_helper.rb
@@ -0,0 +1,25 @@
+module SessionsHelper
+  
+  def login(user)
+    session[:user_id] = user.id
+  end
+  
+  def logout
+    session.delete(:user_id)
+    @current_user = nil
+  end
+  
+  #現在ログインしているユーザー
+  def current_user
+    if session[:user_id]
+      @current_user ||= User.find_by(id: session[:user_id])
+    end
+  end
+  
+  #ログインしていればtrueを返す
+  def logged_in?
+    !current_user.nil?
+  end
+    
+  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   
+  get 'sessions/new'
   get 'users/new'
   get 'user/new'
   root 'static_pages#home'
@@ -10,6 +11,10 @@ Rails.application.routes.draw do
   get '/contact', to:'static_pages#contact'
   
   get '/signup', to:'users#new'
+  
+  get '/login', to:"sessions#new"
+  post '/login', to:"sessions#create"
+  delete '/logout', to:"sessions#destroy"
   
   resources :users
 end
