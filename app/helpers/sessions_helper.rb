03c9040b783b43ab702fba3f25ef2c81c07d0319
kohki <kohki4115@gmail.com>
2020-05-18 03:53:32 +0000

    Finish user actions

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -37,9 +37,25 @@ module SessionsHelper
     end
   end
   
+  #渡されたユーザーがカレントユーザーか
+  def current_user?(user)
+    user && user == current_user
+  end
+  
   #ログインしていればtrueを返す
   def logged_in?
     !current_user.nil?
+  end
+  
+  # 記憶したURL (もしくはデフォルト値) にリダイレクト
+  def redirect_back_or(default)
+    redirect_to(session[:forwarding_url] || default)
+    session.delete(:forwarding_url)
+  end
+
+  # アクセスしようとしたURLを覚えておく
+  def store_location
+    session[:forwarding_url] = request.original_url if request.get?
   end
     
   
